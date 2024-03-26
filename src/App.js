bcd9846fae5b72448cea95c4253572b08857d165
Aniruddha Bhamare <Aniruddha@Aniruddhas-MacBook-Pro.local>
2024-03-26 14:56:13 +0530

    cra & tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="p-2 m-2 text-center font-mono text-green-400 text-2xl">
+        Namaste Everyone, Let's build Netflix GPT.
+      </div>
     </div>
   );
 }
